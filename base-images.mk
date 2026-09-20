13fd0b72d07aed8bed889a5d8c807adf465d0b2c
Wodby Robot <robot@wodby.com>
2026-09-20 16:47:21 +0000

    Refresh Backup base image
    wodby/alpine:latest@sha256:909292f4946048f994caef1e80dffb9508af29bca97dc47f8b738c3e9be30c44 -> wodby/alpine:latest@sha256:93b0e40910eb242dbd3964fc41a84d5ebc635b414796c98843ff4da07bbb9eb0

--- base-images.mk
+++ base-images.mk
@@ -3,7 +3,7 @@
 BASE_IMAGE_REPOSITORY := wodby/alpine
 BASE_IMAGE_VERSION_SUFFIX :=
 
-BASE_IMAGE_DIGEST_latest := sha256:909292f4946048f994caef1e80dffb9508af29bca97dc47f8b738c3e9be30c44
+BASE_IMAGE_DIGEST_latest := sha256:93b0e40910eb242dbd3964fc41a84d5ebc635b414796c98843ff4da07bbb9eb0
 
 # Fail before building when a version or variant has no reviewed pin.
 BASE_IMAGE = $(BASE_IMAGE_REPOSITORY):$(BASE_IMAGE_TAG)@$(or $(BASE_IMAGE_DIGEST_$(BASE_IMAGE_TAG)),$(error No base image digest for $(BASE_IMAGE_REPOSITORY):$(BASE_IMAGE_TAG); update base-images.mk))
