cd7531b2f71d889d8fa56efc0103405eb3d57ffe
Wodby Robot <robot@wodby.com>
2026-09-24 07:03:10 +0000

    Rebuild against updated base image digests
    wodby/alpine:3@sha256:36d576ef6714d14631aa75efa5c0170665f86e52af82fa6e65357edaa4b356a0 -> wodby/alpine:3@sha256:949c78543d735ed2ea56a8b02e5d3cbe3faff961bacf425cd725cdf6c853927c

--- base-images.mk
+++ base-images.mk
@@ -3,7 +3,7 @@
 BASE_IMAGE_REPOSITORY := wodby/alpine
 BASE_IMAGE_VERSION_SUFFIX :=
 
-BASE_IMAGE_DIGEST_3 := sha256:36d576ef6714d14631aa75efa5c0170665f86e52af82fa6e65357edaa4b356a0
+BASE_IMAGE_DIGEST_3 := sha256:949c78543d735ed2ea56a8b02e5d3cbe3faff961bacf425cd725cdf6c853927c
 BASE_IMAGE_DIGEST_3-r0 := sha256:a86e97a7768e937427a8768adc5b20935c60fd06a2c994fbccd1e9a5d569b6a6
 
 # Fail before building when a version or variant has no reviewed pin.
